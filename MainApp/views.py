76b2d660ce4d186ad863579841fadf6471ea3511
Aleksandra Peskova <peskoval88@gmail.com>
2024-07-16 12:11:25 +0300

    partly done HW day1

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,10 +2,26 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+fio = 'Пескова А.О.'
+name = 'Александра'
+secname = 'Олеговна'
+surname = 'Пескова'
+phone = '8-9**-211-31-18'
+email= 'peskoval**@gmail.com'
 
 def home(request):
-    text = """
+    text = f"""
     <h1>"Изучаем django"</h1>
-    <strong>Автор</strong>: <i>Иванов И.П.</i>
+    <strong>Автор</strong>: <i>{fio}</i>
     """
+    return HttpResponse(text)
+
+def about(request):
+    text = f'''
+    Имя: {name}<br>
+    Отчество: {secname}<br>
+    Фамилия: {surname}<br>
+    телефон: {phone}<br>
+    email: {email}'''
+
     return HttpResponse(text)
