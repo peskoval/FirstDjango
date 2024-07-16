cff82dd8c299bc16ccb842c54fe40c9af1762cf3
Aleksandra Peskova <peskoval88@gmail.com>
2024-07-16 18:26:34 +0300

    with items

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from MainApp import views
 
 urlpatterns = [
     path('', views.home),
     path('about/', views.about),
+    path('items/', include('MainApp.urls')),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -9,12 +9,23 @@ surname = 'Пескова'
 phone = '8-9**-211-31-18'
 email= 'peskoval**@gmail.com'
 
+
+items = [
+{"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+{"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+{"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+{"id": 7, "name": "Картофель фри" ,"quantity":0},
+{"id": 8, "name": "Кепка" ,"quantity":124},
+]
+
+
 def home(request):
-    text = f"""
+    text = f'''
     <h1>"Изучаем django"</h1>
     <strong>Автор</strong>: <i>{fio}</i>
-    """
+    '''
     return HttpResponse(text)
+
 
 def about(request):
     text = f'''
@@ -23,5 +34,19 @@ def about(request):
     Фамилия: {surname}<br>
     телефон: {phone}<br>
     email: {email}'''
-
     return HttpResponse(text)
+
+
+def all_items(request):
+    list_items = [item['name'] for item in items]
+    page = f'''
+    <html><head><title>list of all items</title></head>
+    <body>{list_items[0]}<br>
+    {list_items[1]}<br>
+    {list_items[2]}<br>
+    {list_items[3]}<br>
+    {list_items[4]}
+    </body>
+    </html>
+    '''
+    return HttpResponse(page)
