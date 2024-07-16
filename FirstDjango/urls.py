85e5c507ee63984c117e1027c6a80f517875c54a
Aleksandra Peskova <peskoval88@gmail.com>
2024-07-16 19:33:05 +0300

    day2

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from MainApp import views
+
 
 urlpatterns = [
     path('', views.home),
-    path('about/', views.about),
-    path('items/', include('MainApp.urls')),
+    path('about', views.about),
+    path('item/<int:item_id>', views.get_item),
+    path('items', views.get_items),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,52 +1,65 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
-fio = 'Пескова А.О.'
-name = 'Александра'
-secname = 'Олеговна'
-surname = 'Пескова'
-phone = '8-9**-211-31-18'
-email= 'peskoval**@gmail.com'
 
+author = {
+    "Имя": "Александра",
+    "Отчество": "Олеговна",
+    "Фамилия": "Пескова",
+    "телефон": "8-9**-211-31-18",
+    "email": "peskoval**@gmail.com"
+}
 
 items = [
-{"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-{"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-{"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-{"id": 7, "name": "Картофель фри" ,"quantity":0},
-{"id": 8, "name": "Кепка" ,"quantity":124},
+   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+   {"id": 7, "name": "Картофель фри" ,"quantity":0},
+   {"id": 8, "name": "Кепка" ,"quantity":124},
 ]
 
 
 def home(request):
-    text = f'''
+    text = """
     <h1>"Изучаем django"</h1>
-    <strong>Автор</strong>: <i>{fio}</i>
-    '''
+    <strong>Автор</strong>: <i>Пескова А.О.</i>
+    """
     return HttpResponse(text)
 
 
 def about(request):
-    text = f'''
-    Имя: {name}<br>
-    Отчество: {secname}<br>
-    Фамилия: {surname}<br>
-    телефон: {phone}<br>
-    email: {email}'''
+    text = f"""
+    Имя: {author["Имя"]}<br>
+    Отчество: {author["Отчество"]}<br>
+    Фамилия: {author["Фамилия"]}<br>
+    телефон: {author['телефон']}<br>
+    email: {author["email"]}
+    """
     return HttpResponse(text)
 
 
-def all_items(request):
-    list_items = [item['name'] for item in items]
-    page = f'''
-    <html><head><title>list of all items</title></head>
-    <body>{list_items[0]}<br>
-    {list_items[1]}<br>
-    {list_items[2]}<br>
-    {list_items[3]}<br>
-    {list_items[4]}
-    </body>
-    </html>
-    '''
-    return HttpResponse(page)
+# /item/1 
+# /item/2
+# ...
+# /item/n
+def get_item(request, item_id):
+    """ По указанному id возвращает элемент из списка. """
+    for item in items:
+        if item['id'] == item_id:
+            result = f"""
+            <h2> Имя: {item['name']} </h2>
+            <p> Количество: {item['quantity']} </p>
+            <p> <a href='/items'>Назад к списку товаров</a> </p>
+            """
+            return HttpResponse(result)
+    return HttpResponseNotFound(f"Товар с id={item_id} не найден")
+
+
+def get_items(request):
+    result = "<h1> Список товаров </h1><ol>" 
+    for item in items:
+        result += f"""<li><a href='/item/{item["id"]}'> {item["name"]} </a></li>"""
+    result += "</ol>"
+    return HttpResponse(result)
+       
