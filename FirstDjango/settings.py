a671fd2b7ac88e1f23df13e92048ccea514fa1b7
Aleksandra Peskova <peskoval88@gmail.com>
2024-07-19 18:47:27 +0300

    po sledam 4etverga

--- FirstDjango/settings.py
+++ FirstDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c%zbf1a&cepatl-$y7%a01w*60voeg1y+9p+z4h)2f4xy8(gtq'
+SECRET_KEY = 'django-insecure-7@n&-z9nsluby5o!u!-yu)bf-us22=*ddmkr@bm#=-iu^(s#eg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'MainApp',
-    'django_extensions',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / "static",
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
